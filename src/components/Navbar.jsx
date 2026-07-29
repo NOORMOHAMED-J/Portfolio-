23322069fe80ad664d930be1955204d3b11f4c94
NOOR MOHAMED J <jalal15368@gmail.com>
2026-07-29 14:04:01 +0530

    Uncomment React import in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-//import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { personalInfo } from '../data/portfolioData';
 
 const Navbar = () => {
